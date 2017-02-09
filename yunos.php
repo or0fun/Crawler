18a19b2d66817447f529f03ece1ea2f44ae84899
路飞 <lufei@showjoy.com>
2017-02-10 00:55:06 +0800

    界面

--- yunos.php
+++ yunos.php
@@ -4,7 +4,8 @@
 		<script src="http://libs.baidu.com/jquery/2.0.0/jquery.min.js"></script>
 		<style type="text/css">
 			body{
-				text-align:center;
+				text-align:left;
+				margin-left: 300px;
 			}
 			.s_btn {
 				width: 250px;
@@ -21,7 +22,18 @@
 				cursor: pointer;
 			}
 			.s_ipt {
-			    width: 526px;
+			    width: 426px;
+			    height: 22px;
+			    font: 16px/22px arial;
+			    margin: 6px 0 0 7px;
+			    padding: 0;
+			    background: transparent;
+			    border: 0;
+			    outline: 0;
+			    -webkit-appearance: none;
+			}
+			.from {
+			    width: 126px;
 			    height: 22px;
 			    font: 16px/22px arial;
 			    margin: 6px 0 0 7px;
@@ -41,11 +53,21 @@
 			    background: #fff;
 			    display: inline-block;
 			    vertical-align: top;
-			    width: 539px;
+			    width: 439px;
 			    height: 32px;
-			    margin-right: 0;
-			    border-right-width: 0;
-			    border-color: #b8b8b8 transparent #ccc #b8b8b8;
+			    margin-right: 2;
+			    border-color: #b8b8b8 #b8b8b8 #ccc #b8b8b8;
+			}
+			.s_ipt_from {
+			    border: 1px solid #b6b6b6;
+			    border-color: #7b7b7b #b6b6b6 #b6b6b6 #7b7b7b;
+			    background: #fff;
+			    display: inline-block;
+			    vertical-align: top;
+			    width: 139px;
+			    height: 32px;
+			    margin-right: 10;
+			    border-color: #b8b8b8 #b8b8b8 #ccc #b8b8b8;
 			}
 		</style>
 
@@ -65,8 +87,12 @@
 		<br/>
 		<br/>
 
+		<span class="s_ipt_from">
+			<input name="fromdate" id="fromdate" class="from" value="2016-01-01" maxlength="100" autocomplete="on">
+		</span>
 		<span class="s_ipt_wr">
-			<input name="word" id="words" class="s_ipt" value="YunOS" maxlength="100" autocomplete="off">
+			<input name="word" id="words" class="s_ipt" value="YunOS" maxlength="100" autocomplete="on">
+			
 		</span>
 		<span class="s_btn_wr">
 			<button id="crawler" class="s_btn">爬一下</button>
@@ -75,19 +101,29 @@
 	
 		<br/>
 		<br/>
+
+
+		1、起始时间格式为yyyy-mm-dd，如：2016-02-03<br/>
+		2、关键词，多个时以空格隔开
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
 		<br/>
 		<?php
-		$current_dir = 'Crawler';
-		$dir = opendir($current_dir);
-		echo "历史记录:";
-		echo "<ul>";
-		while(false !== ($file=readdir($dir))){
-			if($file != "." && $file != ".." && strstr($file, ".xls")){
-				echo "<li><a href='http://ie8384.com/pudding/Crawler/$file'>$file</a></li>";
+			$current_dir = 'Crawler';
+			$dir = opendir($current_dir);
+			echo "历史记录:<br/>";
+			while(false !== ($file=readdir($dir))){
+				if($file != "." && $file != ".." && strstr($file, ".xls")){
+					echo "<a href='http://ie8384.com/pudding/Crawler/$file'>$file</a>";
+					$ctime = filectime("Crawler/".$file);
+					echo "        Created:".date("Y-m-d H:i:s",$ctime);
+					echo "<br/>";
+				}
 			}
-		}
-		echo "</ul>";
-		closedir($dir);
+			closedir($dir);
 		?>
 	</body>
 </html>
